66d0724d70e22087ada2dc5178cc42ad89238665
Coleen Masbang <coleenmasbang.asi@gmail.com>
2024-05-11 17:34:46 +0800

    Angular Web Project - Whistle Street

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'prelim-project-cimasbang';
+  title = 'Routing';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProductComponent } from './product/product.component';
 
-export const routes: Routes = [];
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+
+// asterisk for pagenotfound 
+
+export const routes: Routes = [
+    {"path":'', "title":"Home", component:HomeComponent},
+    {"path":'about', "title":"About Us", component:AboutComponent},
+    {"path":'product', "title":"Product", component:ProductComponent},
+    {"path":'**', "title":"Page Not Found", component:PagenotfoundComponent}
+];
